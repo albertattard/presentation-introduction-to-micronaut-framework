9022d7f93f64f8e18e4c53142a32d37653b8cc13
Albert Attard <albert.attard@thoughtworks.com>
2020-04-19 18:48:55 +0200

    Build run application

--- run-kotlin-demo/src/main/kotlin/com/albertattard/presentation/Application.kt
+++ run-kotlin-demo/src/main/kotlin/com/albertattard/presentation/Application.kt
@@ -8,6 +8,8 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.net.http.HttpResponse.BodyHandlers
 import java.util.concurrent.TimeUnit
+import kotlin.system.exitProcess
+import kotlin.system.measureTimeMillis
 import org.slf4j.LoggerFactory
 
 object Application {
@@ -16,11 +18,19 @@ object Application {
 
     @JvmStatic
     fun main(args: Array<String>) {
+        val application = when (val flag = args.firstOrNull() ?: "-m") {
+            "-m" -> "micronaut-kotlin-demo/build/libs/micronaut-kotlin-demo-1.0-all.jar"
+            else -> {
+                LOGGER.error("Unknown application flag '{}'", flag)
+                exitProcess(-1)
+            }
+        }
+
         LOGGER.debug("Starting application")
         val process = ProcessBuilder()
             .inheritIO()
             .directory(File("."))
-            .command("java", "-jar", "micronaut-kotlin-demo/build/libs/micronaut-kotlin-demo-1.0-all.jar")
+            .command("java", "-jar", application)
             .start()
 
         val client: HttpClient = HttpClient.newHttpClient()
@@ -28,18 +38,21 @@ object Application {
             .uri(URI.create("http://localhost:8080/contacts/"))
             .build()
 
-        loop@ for (i in 1..25) {
-            try {
-                LOGGER.debug("Checking")
-                val response: HttpResponse<String> = client.send(request, BodyHandlers.ofString())
-                LOGGER.debug("Response {}", response.body())
-                break@loop
-            } catch (e: ConnectException) {
-                LOGGER.warn("Failed to connect")
-            }
+        val timeToFirstResponse = measureTimeMillis {
+            loop@ for (i in 1..25) {
+                try {
+                    LOGGER.debug("Checking")
+                    val response: HttpResponse<String> = client.send(request, BodyHandlers.ofString())
+                    LOGGER.debug("Response {}", response.body())
+                    break@loop
+                } catch (e: ConnectException) {
+                    LOGGER.warn("Failed to connect")
+                }
 
-            TimeUnit.MILLISECONDS.sleep(100)
+                TimeUnit.MILLISECONDS.sleep(100)
+            }
         }
+        LOGGER.debug("Application took {} milliseconds to reply", timeToFirstResponse)
 
         LOGGER.debug("Stopping the application")
         process.destroy()
